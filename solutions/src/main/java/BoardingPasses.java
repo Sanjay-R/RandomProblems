bdd105ab0dcbee2f58f9775b3793750efc5e74d2
Sanjay-R <sanjayrachwani@yahoo.com>
2019-12-23 22:04:01 +0100

    Added the first class and its description & constructor.

---
+++ solutions/src/main/java/BoardingPasses.java
@@ -0,0 +1,18 @@
+/**
+ * Given that you have a bunch of boarding passes but you drop them to the floor and their order gets messed up.
+ * You depart off from one city, having layovers until eventually you reach your end city.
+ * Re-order them (from starting city to end)!
+ * There are no loops (no cities that are traveled twice).
+ */
+public class BoardingPasses {
+
+    String[] all;
+
+    public BoardingPasses(String[] all) {
+        this.all = all;
+    }
+
+    public String[] solve(String[] all) {
+        return null;
+    }
+}
