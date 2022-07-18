2a670441edac1e29afe6dd21ab00fa77a009003f
Sanjay R <sanjayrachwani@yahoo.com>
2022-07-19 00:41:45 +0200

    It does not work

--- solutions/src/main/java/BTFromPreorderAndPostorder.java
+++ solutions/src/main/java/BTFromPreorderAndPostorder.java
@@ -48,42 +48,7 @@ public class BTFromPreorderAndPostorder {
                 return new TreeNode(inorder[0]);
             }
 
-            return divide(preorder, inorder, 0, preorder.length - 1);
-        }
-
-        public TreeNode divide(int[] pre, int[] in, int l, int r) {
-            if (l > r) {
-                //case of no nodes left
-                return null;
-            } else if (l == r) {
-                //case of a single node left
-                return new TreeNode(pre[l]);
-            } else if (l + 1 == r) {
-                //case of two nodes left
-                TreeNode root = new TreeNode(pre[l]);
-                //check if there is only a root and a right side
-                if(pre[l] == in[l]) {
-                    root.right = new TreeNode(in[r]);
-                } else {
-                    root.left = new TreeNode(in[l]);
-                }
-                return root;
-            } else {
-                //default case
-                //create root
-                TreeNode root = new TreeNode(pre[l]);
-
-                int pointerVal = root.val; // == pre[l]
-                int iter = l;
-                while(pointerVal != in[iter] && iter <= r) {
-                    iter++;
-                }
-
-                //conquer on each side
-                root.left = divide(pre, in, (l + 1), iter);
-                root.right = divide(pre, in, (iter + 1), r);
-                return root;
-            }
+            return divide(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
         }
 
         public TreeNode divide(int[] pre, int[] in) {
@@ -122,6 +87,42 @@ public class BTFromPreorderAndPostorder {
                     int[] in3 = Arrays.copyOfRange(in, (iter + 1), in.length);
                     root.right = divide(pre3, in3);
                     return root;
+            }
+        }
+
+        // This one does not work
+        public TreeNode divide(int[] pre, int[] in, int l, int r, int l_in, int r_in) {
+            if (l > r) {
+                //case of no nodes left
+                return null;
+            } else if (l == r && l > 0) {
+                //case of a single node left
+                return new TreeNode(pre[l]);
+            } else if (l + 1 == r) {
+                //case of two nodes left
+                TreeNode root = new TreeNode(pre[l]);
+                //check if there is only a root and a right side
+                if(pre[l] == in[l_in]) {
+                    root.right = new TreeNode(in[r_in]);
+                } else {
+                    root.left = new TreeNode(in[l_in]);
+                }
+                return root;
+            } else {
+                //default case
+                //create root
+                TreeNode root = new TreeNode(pre[l]);
+
+                int pointerVal = root.val; // == pre[l]
+                int iter = l_in;
+                while(pointerVal != in[iter] && iter <= r_in) {
+                    iter++;
+                }
+
+                //conquer on each side
+                root.left = divide(pre, in, (l + 1), iter, l_in, (iter - 1));
+                root.right = divide(pre, in, (iter + 1), r, (iter + 1), r_in);
+                return root;
             }
         }
     }
